7030a27b2b6500142eef312689ca1f1b965e8786
shaniedulab <shani@edulab.in>
2023-02-22 11:10:39 +0530

    3rd commit\

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express=require('express');
-const  config = require('./config/default');
-const app=express()
-const PORT=config.api.PORT;
-const {Sequelize,DataTypes} = require("sequelize");
+const express = require('express');
+const config = require('./config/default');
+const app = express()
+const PORT = config.api.PORT;
+const {
+    Sequelize,
+    DataTypes
+} = require("sequelize");
 const bodyParser = require("body-parser");
 var jwt = require('jsonwebtoken');
-const cors=require('cors')
+const cors = require('cors')
 const cookieparser = require('cookie-parser');
-const router=require('./test');
-
+const router = require('./test');
+const bcrypt = require('bcrypt');
 
 app.use(cors({
     credentials: true,
@@ -19,168 +22,196 @@ app.use(router)
 
 
 const sequelize = new Sequelize(
- 'jwt',
- 'root',
- '1234',
-  {
-    host: 'localhost',
-    dialect: 'mysql'
-  }
+    'jwt',
+    'root',
+    '1234', {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
 );
 sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
- }).catch((error) => {
+}).catch((error) => {
     console.error('Unable to connect to the database: ', error);
- });
+});
 
-const register=sequelize.define('register',{
-    id:{
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
+const register = sequelize.define('register', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
-    username:{
-        type:DataTypes.STRING,
-        allowNull:false
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    email:{
-        type:DataTypes.STRING,
-        allowNull:false
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    password:{
-        type:DataTypes.STRING,
-        allowNull:false
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 
-},  {
-sequelize,
-tableName: 'register',
-timestamps: true,
-createdAt: "created_at", // alias createdAt as created_at
-updatedAt: "updated_at",
-indexes: 
-[
-  {
-    name: "PRIMARY",
-    unique: true,
-    using: "BTREE",
-    fields: [
-      { name: "id" },
-    ]
-  },
-]
+}, {
+    sequelize,
+    tableName: 'register',
+    timestamps: true,
+    createdAt: "created_at", // alias createdAt as created_at
+    updatedAt: "updated_at",
+    indexes: [{
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [{
+            name: "id"
+        }, ]
+    }, ]
 });
 
-const todo=sequelize.define('todo',{
-    id:{
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
+const todo = sequelize.define('todo', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
-    todo:{
-        type:DataTypes.STRING,
-        allowNull:false
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    user_id:{
-        type:DataTypes.INTEGER,
-        allowNull:false
+    auther:{
+        type: DataTypes.STRING,
+        allowNull: false
     },
-},  {
-sequelize,
-tableName: 'todo',
-timestamps: true,
-createdAt: "created_at", // alias createdAt as created_at
-updatedAt: "updated_at",
-indexes: 
-[
-  {
-    name: "PRIMARY",
-    unique: true,
-    using: "BTREE",
-    fields: [
-      { name: "id" },
-    ]
-  },
-]
+    image:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    tableName: 'todo',
+    timestamps: true,
+    createdAt: "created_at", // alias createdAt as created_at
+    updatedAt: "updated_at",
+    indexes: [{
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [{
+            name: "id"
+        }, ]
+    }, ]
 });
 
 
- sequelize.sync().then(() => {
+sequelize.sync().then(() => {
     console.log('Book table created successfully!');
- }).catch((error) => {
+}).catch((error) => {
     console.error('Unable to create table : ', error);
- });
+});
 
-app.post('/postRegister', async (req, res) =>{
-    console.log("body data is ==>",req.body)
-    try{
-        if(req.body.email){
-            const findEmail=  await register.findOne({where:{ email:req.body.email}})
-            if(findEmail){
-            res.json({
-                status:400,
-                message:'Email already exists'
+app.post('/postRegister', async (req, res) => {
+    try {
+        if (req.body.email) {
+            const findEmail = await register.findOne({
+                where: {
+                    email: req.body.email
+                }
             })
-            }else{
-            const Register =await register.create({
-                username:req.body.username,
-                email:req.body.email,
-                password:req.body.password
-            })
-            if(Register){
-                res.status(200).json({
-                    status:200,
-                    data:Register
-
-                })
-            }else{
+            if (findEmail) {
                 res.json({
-                    status:400,
-                    message:'Unable to register'
+                    status: 400,
+                    message: 'Email already exists'
                 })
+            } else {
+                await bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        console.log('error occured in password hashing:', err.message)
+                    } else {
+
+                        const Register = register.create({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        })
+                        if (Register) {
+                            res.status(200).json({
+                                status: 200,
+                                data: Register
+
+                            })
+                        } else {
+                            res.json({
+                                status: 400,
+                                message: 'Unable to register'
+                            })
+                        }
+                    }
+                })
+
             }
-        }   
+        }
+
+    } catch (error) {
+        console.log("error is ", error)
     }
 
-    } catch(error){
-        console.log("error is " , error)
-    }
-    
 })
 
-app.post('/login', async (req, res) =>{
-    console.log("body data is ==>",req.body)
-    try{
-        const data= await register.findOne({
-            where:{
+app.post('/login', async (req, res) => {
+    try {
+
+        const data = await register.findOne({
+            where: {
                 email: req.body.email,
-                password: req.body.password }
-            })  
-            if(!data){
-                res.json({
-                    status:400,
-                    message:'Unable to login'})
-            }else{
-                    req.user =data.id
-                    console.log(req.user)
-                    const userID =req.user
-                    module.exports.userID =userID
-            jwt.sign({data},"secretkey",{expiresIn:'2h'},(err ,token)=>{
-                if(token){
-                    res.json({
-                        status:200,
-                        data:token })
+            }
+        })
+        if (!data) {
+            res.json({
+                status: 400,
+                message: 'Unable to login'
+            })
+        } else {
+            const result=await bcrypt.compare(req.body.password, data.password)
+               if(result){
+                    jwt.sign({data}, "secretkey", {expiresIn: '2h'},(err, token) => {
+                        if (token) {
+                            req.user = data.id
+                            const userId = req.user
+                            console.log(userId)
+                            module.exports.userId = userId
+                            res.json({
+                                status: 200,
+                                data: token
+                            })
+                        } else {
+                            res.json({
+                                status: 401,
+                                message: 'Unauthorized'
+                            })
+                        }
+                    })
                 }else{
                     res.json({
-                        status:401,
-                        message:'Unauthorized'
+                        status: 401,
+                        message: 'Unauthorized'
                     })
                 }
-                })
             }
-   }catch(err){
-        console.log('Unable to Login',err.message)
-   }
+            
+        
+    } catch (err) {
+        console.log('Unable to Login', err.message)
+    }
 
     // try{
     //     const loginData=await register.findOne({
@@ -194,13 +225,13 @@ app.post('/login', async (req, res) =>{
     //             res.cookie('jwt',reffreshToken,{ httpOnly: true, 
     //                 sameSite: 'None', secure: true, 
     //                 maxAge: 24 * 60 * 60 * 1000 })
-                
+
     //              res.json({
     //                     status:200,
     //                     data:accessToken,
     //                     cookie:reffreshToken
     //                 })
-               
+
     //         }else{
     //             res.json({'stasus':406,
     //             message: 'Invalid credentials' });
@@ -208,63 +239,37 @@ app.post('/login', async (req, res) =>{
     // }catch(error){
     //     console.log("error is /login", error.message)
     // }
-    
+
 })
 
-// app.post('/addTodo',async (req, res) =>{
-//     console.log("addtodo,re.body")
-//     console.log("addtodo,re.body",req.userId)
 
-//     try{
-//         const addtodo=await todo.create({
-//             todo:req.body.addtodo,
-//             user_id:req.userId
-//         })
-//         console.log(addtodo)
-//         if(addtodo){
-//             res.json({
-//                 status:200,
-//                 data:addtodo
-//             })
+// app.get('/welcome',verifyToken,(req,res)=>{
+//     jwt.verify(req.token,"secretkey",(err,token)=>{
+//         console.log(req.token)
+//         if(err){
+//             res.send("failed to access ")
 //         }else{
-//             res.json({
-//                 status:400,
-//                 message:'todo not added successfully'
-//             })
+//              res.send("Welcome")
 //         }
-//     }catch(error){
-//         console.log("error is /addTodo", error.message)
-//     }
+//     })
+
 // })
+// function verifyToken(req, res, next) {
+//     const bearerHeader = req.headers['authorization'];
+//     if(typeof bearerHeader !== 'undefined') {
+//       const bearer = bearerHeader.split(' ');
+//       const bearerToken = bearer[1];
+//       req.token = bearerToken;
+//       next();
+//     } else {
+//       res.sendStatus(403);
+//     }
+//   }
 
 
-app.get('/welcome',verifyToken,(req,res)=>{
-    jwt.verify(req.token,"secretkey",(err,token)=>{
-        console.log(req.token)
-        if(err){
-            res.send("failed to access ")
-        }else{
-             res.send("Welcome")
-        }
-    })
-    
-})
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-    if(typeof bearerHeader !== 'undefined') {
-      const bearer = bearerHeader.split(' ');
-      const bearerToken = bearer[1];
-      req.token = bearerToken;
-      next();
-    } else {
-      res.sendStatus(403);
-    }
-  }
-  
-  
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
-module.exports=register
+module.exports.todo = todo
--- test.js
+++ test.js
@@ -1,11 +1,31 @@
 const express=require('express');
 const router = require('express').Router();
-let server=require('./server')
+let index=require('./server')
 
 
-router.post('/addTodo', function(req, res){
-    console.log("userid",server.userId)
-    console.log(req.body)
+
+router.post('/addTodo', async (req, res)=>{
+    req.id = index.userId
+    try{
+        const addtodo=await index.todo.create({
+            todo:req.body.addtodo,
+            user_id:req.id
+        })
+        console.log(addtodo)
+        if(addtodo){
+            res.json({
+                status:200,
+                data:addtodo
+            })
+        }else{
+            res.json({
+                status:400,
+                message:'todo not added successfully'
+            })
+        }
+    }catch(error){
+        console.log("error is /addTodo", error.message)
+    } 
 })
 
 module.exports = router
